8dc5e5d63ef24e8068e2e9004e6929c1d2999d87
Daniel Bustamante Ospina <dbustamante70@gmail.com>
2026-06-10 21:48:26 -0500

    Add Gemma4 Processor to tokenizer.py file

--- verl/utils/tokenizer.py
+++ verl/utils/tokenizer.py
@@ -224,6 +224,8 @@ def hf_processor(name_or_path, **kwargs):
                 model_class = Glm4vModel
             case "MllamaProcessor":
                 pass  # MllamaProcessor and MllamaModel doesn't have get_rope_index property
+            case "Gemma4Processor":
+                pass  # Gemma4Processor doesn't have get_rope_index property
             case _:
                 raise ValueError(f"Unsupported processor type: {processor.__class__.__name__}")
 
